c6299a657a27a61c1a30061645fbf067c41ef603
Nilay Shrimanwar <o788537@cxloyaltycorp.com>
2023-05-16 15:08:55 +0530

    update script

--- updater.py
+++ updater.py
@@ -1,9 +1,11 @@
 import boto3
 import os
 import json
+import sys
 
 bucket = os.environ["BUCKET"]
 sid = os.environ["SID"]
+client = boto3.client("s3")
 
 tags_json = {
     "Name": os.environ["NAME"],
@@ -16,15 +18,17 @@ tags_json = {
     "ProductOwner": os.environ["PRODUCT_OWNER"]
 }
 
-client = boto3.client("s3")
+def update_tag():
+    try:
+        client.put_object(
+            Bucket = bucket,
+            Key = f"{sid}@cxloyaltycorp.com/tags.json",
+            Body = (bytes(json.dumps(tags_json).encode('UTF-8')))
+        )
+    except BaseException as e:
+        print(e)
+        sys.exit(1)
+    else:
+        print(f"Tags updated successfully for SID - {sid}")
 
-try:
-    client.put_object(
-        Bucket = bucket,
-        Key = f"{sid}@cxloyaltycorp.com/tags.json",
-        Body = (bytes(json.dumps(tags_json).encode('UTF-8')))
-    )
-except BaseException as e:
-    raise(e)
-else:
-    print(f"Tags updated successfully for SID - {sid}")
+update_tag()

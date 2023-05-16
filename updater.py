a451dda3cc0947db5c022b9e5b08fc78b5fd29cd
Nilay Shrimanwar <o788537@cxloyaltycorp.com>
2023-05-16 10:53:35 +0530

    added tag updater script

--- updater.py
+++ updater.py
@@ -1,5 +1,30 @@
 import boto3
 import os
+import json
 
-for name, value in os.environ.items():
-    print("{0}: {1}".format(name, value))
+bucket = os.environ["BUCKET"]
+sid = os.environ["SID"]
+
+tags_json = {
+    "Name": os.environ["NAME"],
+    "AppName": os.environ["APP_NAME"],
+    "BusinessUnit": os.environ["BUSINESS_UNIT"],
+    "Product": os.environ["PRODUCT"],
+    "Backup": os.environ["BACKUP"],
+    "Environment": os.environ["ENVIRONMENT"],
+    "InfraOwner": os.environ["INFRA_OWNER"],
+    "ProductOwner": os.environ["PRODUCT_OWNER"]
+}
+
+client = boto3.client("s3")
+
+try:
+    client.put_object(
+        Bucket = bucket,
+        Key = f"{sid}@cxloyaltycorp.com/tags.json",
+        Body = (bytes(json.dumps(tags_json).encode('UTF-8')))
+    )
+except BaseException as e:
+    raise(e)
+else:
+    print(f"Tags updated successfully for SID - {sid}")
